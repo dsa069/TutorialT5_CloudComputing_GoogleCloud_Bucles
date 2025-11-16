d333dd91f7c77db98ce0c3b69e4d6d0f79cd5087
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2025-11-16 11:16:50 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "gcp-username" {
+  description = "GCP user name"
+}
+
+variable "gcp-project" {
+  description = "GCP project"
+}
+
+variable "gcp-network" {
+  description = "GCP network"
+
+}
